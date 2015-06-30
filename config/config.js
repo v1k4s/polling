9f37a42578da861f9ba99653b4bd2341bd974156
v1k4s <vikas.sharma@clicklabs.co>
2015-06-30 09:58:44 +0530

    All files uploaded

--- config/config.js
+++
@@ -1,8 +0,0 @@
-exports.server = {
-    host : '127.0.0.1',
-    port : 8080
-}
-exports.db = {
-    host : 'localhost',
-    port : 27017
-}
--- index.js
+++ index.js
@@ -1,15 +1,39 @@
 /**
  * Created by Vikas on 6/23/15.
+ * Entry point to the application
  */
 var Hapi = require('hapi');
 var Mongoose = require('mongoose');
 var db = Mongoose.connection;
-db.on('error',console.log('Error occurred connecting to database'));
-db.once('open',console.log("Database Connection successful"));
 var Config = require('./config');
+var Routes = require('./routes');
+
+db.on('error',function(){console.log('Error occurred connecting to database')});
+db.once('open',function(){console.log("Database Connection successful")});
+
+
 var server = new Hapi.Server();
-mongoose.connect('mongodb://'+Config.db.host+':'+Config.db.port+'/pollDb');
+Mongoose.connect('mongodb://'+Config.db.host+':'+Config.db.port+'/pollDb');
 server.connection({host:Config.server.host, port:Config.server.port});
-server.start(function(err){
-    console.log('Server started successfully at ', server.info.uri);
+server.views({
+    engines:{
+        html:require('handlebars')
+    },
+    relativeTo: __dirname,
+    path: './views',
+    layoutPath: './views/layouts',
+    layout: true,
+    partialsPath: './views/partials'
 });
+server.register(require('hapi-auth-cookie'), function (err) {
+
+    server.auth.strategy('session', 'cookie', {
+        password: Config.key.secret,
+        cookie: 'polling-app-cookie',
+        redirectTo: '/login',
+        isSecure: false,
+    });
+});
+
+server.route(Routes);
+server.start(function () {console.info('Server started at ' + server.info.uri);});
